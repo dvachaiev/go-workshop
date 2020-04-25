783ccf9fb288d2ffe0de90e5d82d0282ae9a09aa
Dmytro Vachaiev <dvachaiev@llnw.com>
2020-04-25 12:58:57 +0300

    Added config

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ https://github.com/ilyakaznacheev/go-workshop
 https://github.com/golang-standards/project-layout
 
 https://github.com/avelino/awesome-go
+
+https://12factor.net/ru/
--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -5,18 +5,27 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 
+	"workshop/internal/config"
 	"workshop/internal/handler"
 )
 
 func main() {
+	cfg := config.Server{}
+
+	err := cleanenv.ReadConfig("config.yaml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := chi.NewRouter()
 	h := handler.NewHandler()
 
 	r.Get("/hello", h.Hello)
 
 	log.Print("starting server")
-	err := http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(":8080", r)
 	log.Fatal(err)
 
 	log.Print("shutting down server")
